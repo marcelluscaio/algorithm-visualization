dc2443b0fa968021c2942b6b7dce7b99c5825d01
Caio Cabral <ocaiocabral@gmail.com>
2023-05-27 16:59:17 -0300

    refactor: particiona before quick sort

--- assets/js/quickSort.js
+++ assets/js/quickSort.js
@@ -2,24 +2,11 @@
 //const trocaLugar = require('./encontraMenores');
 import troca from './troca.js';
 
-function quickSort(array, esquerda, direita) {
-  console.log(array, esquerda, direita);
-  if (array.length > 1) {
-    let indiceAtual = particiona(array, esquerda, direita);
-    if (esquerda < indiceAtual - 1) {
-      quickSort(array, esquerda, indiceAtual - 1);
-    }
-    if (indiceAtual < direita) {
-      quickSort(array, indiceAtual, direita)
-    }
-  }
-  return array;
-}
-
 function particiona(array, esquerda, direita) {
   console.log('array', array)
   console.log('esquerda, direita', esquerda, direita)
   let pivo = array[Math.floor((esquerda + direita) / 2)]
+  console.log(pivo)
   let atualEsquerda = esquerda; //0
   let atualDireita = direita; //10
 
@@ -40,6 +27,21 @@ function particiona(array, esquerda, direita) {
   }
   return atualEsquerda;
 }
+
+function quickSort(array, esquerda, direita) {
+  console.log(array, esquerda, direita);
+  if (array.length > 1) {
+    let indiceAtual = particiona(array, esquerda, direita);
+    if (esquerda < indiceAtual - 1) {
+      quickSort(array, esquerda, indiceAtual - 1);
+    }
+    if (indiceAtual < direita) {
+      quickSort(array, indiceAtual, direita)
+    }
+  }
+  return array;
+}
+
 
 //console.log(quickSort(listaLivros, 0, listaLivros.length - 1))
 export default quickSort;
